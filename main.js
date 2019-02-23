90e0ba1043e8b66ae08286456533484303d53a6b
Lubomir Lenard <40657698+lubomir1618@users.noreply.github.com>
2019-02-23 20:25:40 +0100

    fetch svg added

--- main.js
+++ main.js
@@ -1,80 +1,39 @@
 // jerseys configuration
-const jerseys = {
-  jersey_halves: {
-    base_color_left: '',
-    base_color_right: '',
-    collar: '',
-    shoulder_left: '',
-    shoulder_right: ''
-  },
-   jersey_hooped: {
-    base_color: '',
-    collar: '',
-    hooped: ''
-  },
-   jersey_quarters: {
-    base_color: '',
-    collar: '',
-    quarters: '',
-    shoulder_left: '',
-    shoulder_right: ''
-  },
+let finalJersey;
+const svgLocation = './jerseys/';
 
-   jersey_plain: {
-    base_color: '',
-    collar: ''
-  },
-   jersey_plain_shoulders: {
-    base_color: '',
-    collar: '',
-    shoulders: '',
-    shoulders_strips: ''
-  },
-   jersey_plain_shoulders_cuffs: {
-    base_color: '',
-    collar: '',
-    shoulders: '',
-    shoulders_strips: '',
-    cuffs: ''
-  },
-
-   jersey_vertical_bars_shoulders: {
-    base_color: '',
-    collar: '',
-    shoulders: '',
-    shoulders_strips: '',
-    bars: '',
-    cuffs: ''
-  },
-   jersey_vertical_bars_shoulders_cuffs: {
-    base_color: '',
-    collar: '',
-    shoulders: '',
-    shoulders_strips: '',
-    bars: '',
-    cuffs: ''
-  },
-   jersey_vertical_bars_cuffs: {
-    base_color: '',
-    collar: '',
-    shoulders: '',
-    shoulders_strips: '',
-    bars: '',
-    cuffs: ''
-  },
+const color_type_names = {
+  main: 'main color (torso)',
+  secondary: 'secondary color',
+  accent: 'collar / shoulders strips / cuffs'
 }
+const jerseys = {
+  jersey_halves: ['main', 'secondary', 'accent'],
+  jersey_hooped: ['main', 'secondary', 'accent'],
+  jersey_quarters: ['main', 'secondary', 'accent'],
 
+  jersey_plain: ['main', 'accent'],
+  jersey_plain_shoulders: ['main', 'secondary', 'accent'],
+  jersey_plain_shoulders_cuffs: ['main', 'accent'],
+
+  jersey_vertical_bars_shoulders: ['main', 'secondary', 'accent'],
+  jersey_vertical_bars_cuffs: ['main', 'secondary', 'accent'],
+  jersey_vertical_bars_shoulders_cuffs: ['main', 'secondary', 'accent'],
+}
 
 // jersey click listener
 let images = document.querySelectorAll('.jersey');
 
 function watchJerseyClick() {
-  const highlight = 'picture-highligt';
+  const highlight = 'picture-highligt';  
+
   [...images].forEach(
     (image) => {
       image.addEventListener('click', () => {
+        let svg = svgLocation + '' + image.id + '.svg';
+
         removeClass(highlight);
-        pasteSVG(image.id);
+        pasteSVG(svg, '#svg');
         drawPicker(image.id)
         image.classList.add(highlight);
       });
@@ -92,23 +51,85 @@ function removeClass(name) {
   );
 };
 
-function pasteSVG(svg) {
-  let svgContainer = document.getElementById('svg');
+function watchPickerClick() {
+  return
+};
 
-  svg = '<img src="./jerseys/' + svg +'.svg" />';
-  svgContainer.innerHTML = svg;
+function pasteSVG(svg, element) {
+  fetch(svg).then(response => {
+    return response.text()
+  }).then(data => {
+    finalJersey = data;
+    document.querySelector(element).innerHTML = finalJersey;
+    getColorsToPicker();
+    svgStyleHandler();
+  })
+}
+
+function changeColorShade(color, percent) {
+  let R = parseInt(color.substring(1, 3), 16);
+  let G = parseInt(color.substring(3, 5), 16);
+  let B = parseInt(color.substring(5, 7), 16);
+
+  R = parseInt(R * (100 + percent) / 100);
+  G = parseInt(G * (100 + percent) / 100);
+  B = parseInt(B * (100 + percent) / 100);
+
+  R = (R < 255) ? R : 255;
+  G = (G < 255) ? G : 255;
+  B = (B < 255) ? B : 255;
+
+  const RR = ((R.toString(16).length == 1) ? '0' + R.toString(16) : R.toString(16));
+  const GG = ((G.toString(16).length == 1) ? '0' + G.toString(16) : G.toString(16));
+  const BB = ((B.toString(16).length == 1) ? '0' + B.toString(16) : B.toString(16));
+
+  return '#' + RR + GG + BB;
+};
+
+function svgStyleHandler() {
+  let style = document.querySelector('#svg').querySelector('svg').getElementsByTagName('style')[0];
+  console.log(style);
+  //debugger;
+}
+
+function getColorsToPicker() {
+  const svg = document.querySelector('svg');
+  const svgRules = svg.getElementsByTagName('style')[0].sheet.cssRules;
+  const cssClasses = [];
+  let form = document.querySelector('#form'); 
+
+  Object.keys(color_type_names).forEach(color => {
+    cssClasses.push(color);
+  });
+
+  [...svgRules].forEach(rule => {
+    const cssSelector = rule.selectorText.substr(1);
+    if(cssClasses.includes(cssSelector)) {
+      form.querySelector('#' + cssSelector).value = rgbToHex(rule.style.fill);
+    }
+  });
+}
+
+function rgbToHex(rgb) { 
+  rgb = rgb.match(/\d+/g);
+
+  return '#' + rgb.map(function (number) {
+    let hex = Number(number).toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  }).join('');
 };
 
 function drawPicker(svg) {
   let pickerContainer = document.getElementById('form');
+  let svgContainer = document.getElementById('svg');
   let picker = '';
 
-  Object.keys(jerseys[svg]).forEach(param => {
-
+  svgContainer.innerHTML = '<p class="loading"></p>';
+  jerseys[svg].forEach(param => {
     picker += `
-      <input type="color" id="${param}" name="${param}" value="#ffffff">
-      <label for="base_color">${param}</label>
-      <br>
+      <input type="color" id="${param}" name="${param}" value="#FFFFFF"/>
+      <label for="base_color">${color_type_names[param]}</label>
+      <br/>
     `
   });
 
@@ -116,10 +137,4 @@ function drawPicker(svg) {
 }
 
 watchJerseyClick();
-
-
-/*
-<input type="color" id="base_color" name="base_color" value="#ffffff">
-<label for="base_color">Base color</label>
-<br>
-*/
+watchPickerClick();
